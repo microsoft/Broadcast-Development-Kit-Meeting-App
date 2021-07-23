b0c4e241f1f3ef1c4655c2b5d33d997a277807c3
Nelson Cristhian Vila <64040734+nelsonsouthworks@users.noreply.github.com>
2021-07-23 12:30:07 -0300

    fix: add scrollbar to app component

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import { StatusCode } from "@/models/global/types";
 import { FrameContexts } from "@/models/teamsContext/type";
 import ConfigPage from "@/views/config-page/ConfigPage";
 import SidePanelPage from "@/views/side-panel-page/SidePanelPage";
-import { AlertMessage } from '@/views/components/AlertMessage';
+import { AlertMessage } from "@/views/components/AlertMessage";
 import PreMeetingPage from "./views/pre-meeting-page/PreMeetingPage";
 import RemovePage from "./views/remove-page/RemovePage";
 
@@ -27,39 +27,51 @@ const App: React.FC = () => {
   microsoftTeams.initialize();
 
   const dispatch = useDispatch();
-  const teamsContext: TeamsContextState = useSelector((appState: AppState) => appState.teamsContext);
-  const config: ConfigState = useSelector((appState: AppState) => appState.config);
+  const teamsContext: TeamsContextState = useSelector(
+    (appState: AppState) => appState.teamsContext
+  );
+  const config: ConfigState = useSelector(
+    (appState: AppState) => appState.config
+  );
 
   useEffect(() => {
     dispatch(loadConfig());
     dispatch(getExtensionContext(microsoftTeams));
   }, []);
 
-  const isExtensionReady = teamsContext.status.code === StatusCode.LOADED && config.status.code === StatusCode.LOADED;
-  const frameContext = teamsContext.values?.frameContext as FrameContexts | undefined;
+  const isExtensionReady =
+    teamsContext.status.code === StatusCode.LOADED &&
+    config.status.code === StatusCode.LOADED;
+  const frameContext = teamsContext.values?.frameContext as
+    | FrameContexts
+    | undefined;
 
-  const renderContent = (isExtensionReady: boolean, frameContext?: FrameContexts) => {
+  const renderContent = (
+    isExtensionReady: boolean,
+    frameContext?: FrameContexts
+  ) => {
     if (!isExtensionReady) {
       return (
-        <Flex vAlign="center" hAlign="center" style={{ height: '100vh' }}>
+        <Flex vAlign="center" hAlign="center" style={{ height: "100vh" }}>
           <Loader label="Loading extension..." labelPosition="below" />
-        </Flex>)
+        </Flex>
+      );
     } else {
       const { SidePanel, PreMeeting, Settings, Remove } = FrameContexts;
       switch (frameContext) {
         case SidePanel:
-          return <SidePanelPage />
+          return <SidePanelPage />;
         case PreMeeting:
-          return <PreMeetingPage />
+          return <PreMeetingPage />;
         case Settings:
-          return <ConfigPage />
+          return <ConfigPage />;
         case Remove:
-          return <RemovePage />
+          return <RemovePage />;
         default:
           return null;
       }
     }
-  }
+  };
 
   return (
     <FluentProvider theme={teamsDarkTheme}>
@@ -67,19 +79,19 @@ const App: React.FC = () => {
         id="app"
         style={{
           width: "100%",
-          minHeight: "100vh",
-          overflow: "hidden",
+          maxHeight: "100vh",
+          overflow: "auto",
+          overflowY: "auto",
+          overflowX: "hidden",
           margin: "auto",
           border: "none",
           display: "flex",
           flexDirection: "column",
-          backgroundColor: "rgb(32, 31, 31)"
+          backgroundColor: "rgb(32, 31, 31)",
         }}
       >
         <AlertMessage />
-        {
-          renderContent(isExtensionReady, frameContext)
-        }
+        {renderContent(isExtensionReady, frameContext)}
       </div>
     </FluentProvider>
   );
